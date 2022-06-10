1f5e9e20c7496b5b833d8942dbed2096156fab52
andikans <andikanursasmito5@gmail.com>
2022-06-10 21:16:20 +0700

    praktikumPEMWEB2

---
+++ praktikum-1/README.md
@@ -0,0 +1,2 @@
+# web-pro-ll-MeyrvanMaulanaNurS-SI06
+This is a repository for collecting Web Programming 2
---
+++ praktikum-CIFramework/praktikum1-CIFramework/application/views/mahasiswa/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+     <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Mahasiswa</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Mahasiswa</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">View Mahasiswa</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+        <h3>Daftar Mahasiswa</h3>
+    <table class="table">
+    <thead>
+    <tr>
+      <th scope="col">No.</th>
+      <th scope="col">ID</th>
+      <th scope="col">NIM</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Gender</th>
+      <th scope="col">IPK</th>
+      <th scope="col">Predikat</th>
+    </tr>
+    </thead>
+    <tbody>
+        <?php
+            $nomor = 1;
+            foreach ($list_mhs as $mhs) {
+            echo '<tr><td>'.$nomor.'</td>';
+            echo '<td>'.$mhs -> id.'</td>';
+            echo '<td>'.$mhs -> nim.'</td>';
+            echo '<td>'.$mhs -> nama.'</td>';
+            echo '<td>'.$mhs -> gender.'</td>';
+            echo '<td>'.$mhs -> ipk.'</td>';
+            echo '<td>'.$mhs -> predikat().'</td> </tr>';
+            $nomor++;
+            }
+        ?>
+    </tbody>
+    </table>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</body>
+</html>
